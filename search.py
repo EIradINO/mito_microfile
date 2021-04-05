d20b8cb8d15663d67ffc98a1c724f7083724f78b
EIradINO <eisukeinoue0107@gmail.com>
2021-04-05 18:25:19 +0900

    Re:INitialize repository

--- search.py
+++ search.py
@@ -10,10 +10,11 @@ text = "どうして空は青いのかがが気になりました"
 char_filters = [UnicodeNormalizeCharFilter()]
 tokenizer = Tokenizer()
 token_filters = [POSKeepFilter(['名詞', '形容詞'])]
-analyzer = Analyzer(char_filters=char_filters, tokenizer=tokenizer, token_filters=token_filters)
+analyzer = Analyzer(char_filters=char_filters,
+                    tokenizer=tokenizer, token_filters=token_filters)
 for token in analyzer.analyze(text):
     word_list.append(token.surface)
-#print(word_list)
+# print(word_list)
 
 data_list = ["空の色が青い理由",
              "曇り空を見てみよう",
@@ -26,22 +27,22 @@ results = []
 for word in word_list:
     results.append([s for s in data_list if word in s])
 
-#print(results)
+# print(results)
 
 kekka = []
 for xx in itertools.combinations(results, 2):
     kyotu = set(xx[0]) & set(xx[1])
     if kyotu:
         kekka.append(list(kyotu))
-#print(list(itertools.chain.from_iterable(kekka)))
+# print(list(itertools.chain.from_iterable(kekka)))
 
 a = []
 for count in data_list:
     a.append(list(itertools.chain.from_iterable(kekka)).count(count))
 
-#print(a)
+# print(a)
 b = np.percentile(a, 75)
-#print(np.percentile(a, 75))
+#print(np.percentile(a, 25))
 
 for final in data_list:
     if list(itertools.chain.from_iterable(kekka)).count(final) >= b:
